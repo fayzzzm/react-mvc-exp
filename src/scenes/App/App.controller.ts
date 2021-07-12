0698c062d5ef7fedb98a9a9540e745eb5ece9342
fayzzzm <m.fayzov1@gmail.com>
2021-07-12 10:10:31 +0500

    Change the blocks value via input value change.

--- src/scenes/App/App.controller.ts
+++ src/scenes/App/App.controller.ts
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { AppView } from "./App.view";
 import "./style.css";
 
 export const App = () => {
+  const [name, setName] = useState("Mustafo");
+
+  const handleChange = (event: { target: { value: string } }) => {
+    const {
+      target: { value },
+    } = event;
+
+    setName(value);
+  };
+
   const credentials = {
     data: {
       stack: "React",
+      name,
+    },
+
+    handlers: {
+      handleChange,
     },
   };
 
--- src/scenes/App/App.view.tsx
+++ src/scenes/App/App.view.tsx
@@ -3,11 +3,16 @@ import logo from "../../logo.svg";
 interface IApp {
   data: {
     stack: string;
+    name: string;
+  };
+  handlers: {
+    handleChange: (event: { target: { value: string } }) => void;
   };
 }
 
-export const AppView = ({ data }: IApp) => {
-  const { stack } = data;
+export const AppView = ({ data, handlers }: IApp) => {
+  const { stack, name } = data;
+  const { handleChange } = handlers;
 
   return (
     <div className="App">
@@ -16,6 +21,12 @@ export const AppView = ({ data }: IApp) => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div className="typo-block">
+          <h3>Write something that the world could see: {name}</h3>
+          <div className="typ-block__input">
+            <input type="text" value={name} onChange={handleChange} />
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
